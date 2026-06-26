c53c00c7976b1946385c9c39f0f32112d5f20577
RatneshSingh21 <ratneshs1340@gmail.com>
2026-06-26 23:41:09 +0530

    UI

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
-import profilePic from "../../public/images/profile/pic.png"
+import profilePic from "../../public/images/profile/pic.PNG"
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Education from '@/components/Education'
